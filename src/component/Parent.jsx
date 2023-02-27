ca49b18faebfe5d596ad3c7e23d16417c894ba15
unknown <screw24@nate.com>
2023-02-27 14:18:50 +0900

    myname modify

--- src/component/Parent.jsx
+++ src/component/Parent.jsx
@@ -2,13 +2,13 @@ import React, { useContext } from "react";
 import { MyContext, TitleContext } from "./Context";
 
 function Parent() {
-  const myname = useContext(MyContext);
+  const myname_aa = useContext(MyContext);
   const title = useContext(TitleContext);
 
   return (
     <div>
       <div>
-        Received: {myname} {myname} {title}
+        Received: {myname_aa} {myname_aa} {title}
       </div>
       <div>여기 수정하지 마세요.</div>
     </div>
